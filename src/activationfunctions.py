abed2d24954d949a91adc8dd0d2afb81496c33db
AbdurRafayAkbar <abdurrafaym10@gmail.com>
2026-09-13 16:47:05 +0500

    Added Linear Activation Function and check it in nueron.py

---
+++ src/activationfunctions.py
@@ -0,0 +1,23 @@
+# In this we are comapring the performance of different activation functions in a neural network. The activation functions we will be comparing are ReLU, Sigmoid, and Tanh.
+#  We will use a simple feedforward neural network architecture and evaluate the performance of each activation function on a classification task.
+# we will use the same architecture and hyperparameters for each activation function to ensure a fair comparison.
+# we will import the activation functions to nueron.py and check what our final result come
+
+
+# Linear Activation Function
+def linear(x):
+    return x
+
+values=[-5,-4,-3,-2,-1,0,1,2,3,4,5]
+
+for value in values:
+    print(f"Linear({value}) = {linear(value)}")
+
+def binary_step(x):
+    if x>0:
+        return 1
+    else:
+        return 0
+
+for value in values:
+    print(f"Binary Step({value}) = {binary_step(value)}")
--- src/neuron.py
+++ src/neuron.py
@@ -1,6 +1,6 @@
 #inputs
 x1=2
-x2=4
+x2=3
 #weights
 w1=0.5
 w2=0.8
@@ -9,3 +9,12 @@ b=2
 #weighted_sum = (x1 * w1) + (x2 * w2) + b
 weighted_sum=(x1*w1)+(x2*w2)+b
 print(f"Weighted sum: {weighted_sum}")
+
+#-------------------------------------------- Liner Activation Function
+from activationfunctions import linear
+
+linear_output = linear(weighted_sum)
+print(f"Linear Activation Output: {linear_output}")
+
+#
+
