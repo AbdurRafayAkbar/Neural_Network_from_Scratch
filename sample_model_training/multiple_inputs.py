2fcb1fa02a235678795c0f017fecc34a949fe552
AbdurRafayAkbar <abdurrafaym10@gmail.com>
2026-09-16 15:55:00 +0500

    Created forward pass and calculated the prediction and loss

--- sample_model_training/multiple_inputs.py
+++ sample_model_training/multiple_inputs.py
@@ -3,7 +3,7 @@ X=np.array([[1,2,3],
             [4,5,6],
             [7,8,9]],dtype=float)
 
-target=np.array([
+Y=np.array([
     [25],
     [50],
     [75]
@@ -12,3 +12,17 @@ target=np.array([
 weights=np.zeros(3)
 bias=0.0
 
+epoch=1000
+learning_rate=0.01
+
+for epoch in range(epoch):
+    total_loss=0
+    for inputs,targets in zip(X,Y):
+
+        #forward pass
+        prediction=np.dot(inputs,weights)+bias
+
+        #loss calculation
+        loss=(prediction-targets)**2
+
+        total_loss+=loss
