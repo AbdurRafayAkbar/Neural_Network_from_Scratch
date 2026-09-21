b54371e42c395970c6963b1b0262c9b09be92b6c
AbdurRafayAkbar <abdurrafaym10@gmail.com>
2026-09-21 16:19:20 +0500

    Added

--- sample_model_training/back_propogation_Numerical.py
+++ sample_model_training/back_propogation_Numerical.py
@@ -7,7 +7,13 @@ lr=0.01
 y=10
 #Forward pass
 predicted=(x*w)+b
+
 #Activation function (ReLU)
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
 from activationfunctions import Relu
 from activationfunctions import Relu_derivative
 a=Relu(predicted)
@@ -33,3 +39,8 @@ db=dloss_dpredicted*drelu_dpredicted*b
 
 w=w-(lr*dw)
 b=b-(lr*db)
+
+#output
+print(f"Updated weight: {w}")
+print(f"Updated bias: {b}")
+print(f"Updated predicted: {predicted}")
