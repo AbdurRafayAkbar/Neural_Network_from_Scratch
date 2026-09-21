b0a450b888252fae36868960c7873d08fee47bec
AbdurRafayAkbar <abdurrafaym10@gmail.com>
2026-09-21 21:55:39 +0500

    Added Relu activatio function

--- sample_model_training/multiple_inputs_backpropogation.py
+++ sample_model_training/multiple_inputs_backpropogation.py
@@ -1,4 +1,12 @@
+#To get importing files
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
 #inputs
+from activationfunctions import Relu, Relu_derivative
+
+
 x1=2
 x2=4
 x3=6
@@ -14,3 +22,19 @@ b=5
 
 #Learning rate
 lr=0.01
+
+#Target
+
+y=20
+
+#Forward pass
+
+z=(x1*w1)+(x2*w2)+(x3*w3)+b
+
+#Relu activation function
+
+a=Relu(z)
+
+print(f"Predicted value: {a}")
+print(f"forward pass value: {z}")
+print(f"Target value: {y}")
