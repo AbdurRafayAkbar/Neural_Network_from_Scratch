d8b20286b15f45721ef351bb89c9d7f53669cd36
AbdurRafayAkbar <abdurrafaym10@gmail.com>
2026-09-23 02:08:16 +0500

    Understan how x and w shoudl be matched

--- NN_using_Numpy/numpy_basics.py
+++ NN_using_Numpy/numpy_basics.py
@@ -25,3 +25,19 @@ print(f" added a + b ={(a+b)}")
 #dot product
 
 print(f"Multiplied a . b = {np.dot(a,b)}")
+
+#What if arrays are like this {2,3},{3,}.How we Multiply it
+
+z=np.array([[2,4,6],
+            [8,10,12]])
+print(f"z shape = {z.shape}")
+
+y=np.array([[3],
+            [6],
+            [9]])
+
+print(f"y shape = {y.shape}")
+
+product=np.dot(z,y)
+print(f"product = {product}")
+print(f"new product = {z @ y} ")
