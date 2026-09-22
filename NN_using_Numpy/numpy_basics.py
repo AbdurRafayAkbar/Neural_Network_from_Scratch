67f699ca55736aceda73ae268aeed99c97fdd79f
AbdurRafayAkbar <abdurrafaym10@gmail.com>
2026-09-23 01:43:44 +0500

    Added 2d array

--- NN_using_Numpy/numpy_basics.py
+++ NN_using_Numpy/numpy_basics.py
@@ -1,6 +1,15 @@
 import numpy as np
 #1d array
-x=np.array([2,3,4,5,6,7])
+x_1=np.array([2,3,4,5,6,7])
 
-print(x.shape)
+print(F" 1d array shape = {x_1.shape}")
+
+
+#2d array
+
+x_2=np.array([[2,4,6,8],
+            [1,3,5,7]])
+
+print(f"2d array shape = {x_2.shape}")
+
 
