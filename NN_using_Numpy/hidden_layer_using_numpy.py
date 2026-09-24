aad23f54c768c11c026688ae16d3a879e5b0cef3
AbdurRafayAkbar <abdurrafaym10@gmail.com>
2026-09-25 00:44:28 +0500

    Learned Numpy in hidden layer working

--- NN_using_Numpy/hidden_layer_using_numpy.py
+++ NN_using_Numpy/hidden_layer_using_numpy.py
@@ -6,6 +6,7 @@ x=np.array([
     [5,6]
 
 ])
+print(f"x shape : {x.shape}")
 
 #3 hidden Layer Neurons
 
@@ -17,8 +18,21 @@ bias=np.array([0.1,0.1,0.1])
 #output layer Neuron weights
 
 w1_out=np.array([
-    [3,4],
-    [5,6],
-    [7,8]
+    [3],
+    [5],
+    [8]
 ])
-b_out=np.array([0.1,0.1,0.1])
+print(f"w1_out shape : {w1_out.shape}")
+b_out=np.array([0.1])
+
+    #FORWARD Prop
+z1=(x@w1)+bias
+print(f"z1 shape = {z1.shape}")
+print(z1)
+
+    #RELU
+
+z2=(z1@w1_out)+b_out
+y_pred=z2
+print(f"forward {z1}")
+print(f"prediction {z2}")
